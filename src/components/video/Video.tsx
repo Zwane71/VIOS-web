405c1ee8cec6f50a2b5b5e739fa3ff78fd0a31d4
MakZwane71 <makhohlisathemba@gmail.com>
2024-10-25 13:36:11 +0200

    changed video ip

--- src/components/video/Video.tsx
+++ src/components/video/Video.tsx
@@ -1,7 +1,7 @@
 export default function Video() {
 	return (
 		<iframe
-			src='http://192.168.0.19:5001/video_feed'
+			src='http://10.200.0.110:5001/video_feed'
 			frameBorder='0'
 			allowFullScreen
 			width={1080}
